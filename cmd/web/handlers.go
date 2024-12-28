7e37ddc8b1452f469692b4655966c3088eb9a3e2
pearlescen7 <uysalu18@hotmail.com>
2024-12-29 00:59:21 +0100

    fix: update snippet ID retrieval to use path parameter

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -20,7 +20,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	id, err := strconv.Atoi(r.URL.Query().Get(":id"))
 	if err != nil || id < 1 {
 		app.notFound(w)
 		return
